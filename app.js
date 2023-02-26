d3f46bb67daec0d457884806aaab6f34a16fb1fc
Mateo Fernandez <mateo.rost@gmail.com>
2023-02-26 14:45:20 -0300

    css y validaciones

--- app.js
+++ app.js
@@ -1,55 +1,108 @@
-let palabrasPosibles = ["ahorcado"];
+let palabrasPosibles = ["ahorcado", "pizza", "camaron", "carniceria", "clasificacion", "ambiguedad", "transito", "caramelera", "anestesia", "programacion"];
 
-let palabra = "ahorcado";
+let palabra = "";
 
 let palabraAdivinada = "";
 
-let intentosDisponibles = 5;
+let intentosDisponibles = 0;
 
-const divIntentos = document.getElementById("intentos");
 const divPalabra = document.getElementById("palabra");
 const btnEnviar = document.getElementById("enviar");
 const letra = document.getElementById("letra");
+const historial = document.getElementById("historial");
+const mensaje = document.getElementById("mensaje");
+const ahorcado = document.getElementById("ahorcado");
+const btnReiniciar = document.getElementById("reiniciar");
+const form = document.getElementById("form");
 
 btnEnviar.addEventListener("click", (event)=> {
     event.preventDefault();
     let letraElegida = letra.value;
     console.log(intentar(letraElegida));
     divPalabra.innerHTML = palabraAdivinada;
-    divIntentos.innerHTML = intentosDisponibles;
+    ahorcado.setAttribute("src", imagen(intentosDisponibles));
+    letra.value = "";
     verificar();
-})
+});
+
+btnReiniciar.addEventListener("click", comenzarJuego);
+
 
 function comenzarJuego() {
-    palabraAdivinada = "-".repeat(palabra.length);
+    palabra = random(palabrasPosibles);
+    palabraAdivinada = "_".repeat(palabra.length);
     divPalabra.innerHTML = palabraAdivinada;
-    intentosDisponibles = 5;
-    divIntentos.innerHTML = intentosDisponibles;
+    intentosDisponibles = 6;
+    btnReiniciar.classList.add("oculto");
+    form.classList.remove("oculto");
+    mensaje.innerHTML = "";
+    historial.innerHTML = "";
+    ahorcado.setAttribute("src", imagen(intentosDisponibles));
 }
 
 function intentar(letra) {
+    mensaje.innerHTML = "";
     let letrasEncontradas = 0;
     let string = Array.from(palabraAdivinada);
+
+    if (!esLetra(letra)) {
+        mensaje.innerHTML = "🚫 Solo se permiten letras"
+        return
+    }
+    
+
+    if (historial.innerHTML.includes(letra.toUpperCase()) || palabraAdivinada.includes(letra.toUpperCase())) {
+        mensaje.innerHTML = "⛔ Esa letra ya fue utilizada"
+        return
+    }
+
     for (let i = 0; i < palabra.length; i++) {
         if (letra === palabra[i]) {
             string[i] = letra;
             letrasEncontradas++;
         }
     }
+
     if (letrasEncontradas === 0) {
+        historial.insertAdjacentText("beforeend",`${letra.toUpperCase()} `);
         intentosDisponibles--;
     }
-    console.log(letrasEncontradas)
     
-    palabraAdivinada = string.toString().replaceAll(',','');
+    palabraAdivinada = string.toString().replaceAll(',','').toUpperCase();
 }
 
 function verificar() {
-    if (palabraAdivinada === palabra) {
-        alert("Ganaste!!!")
+    if (palabraAdivinada.toUpperCase() === palabra.toUpperCase()) {
+        mensaje.innerHTML = "😄 ¡Felicitaciones! ¡Ganaste!";
+        btnReiniciar.classList.remove("oculto");
+        form.classList.add("oculto");
     } else if (intentosDisponibles === 0) {
-        alert("Perdiste!!!")
+        mensaje.innerHTML = `😢 ¡Perdiste! La palabra era "${palabra.toUpperCase()}".`;
+        btnReiniciar.classList.remove("oculto");
+        form.classList.add("oculto");
     }
 }
 
+function imagen (intentos) {
+    switch (intentos) {
+        case 6: return "img/ahorcado_6.png"; 
+        case 5: return "img/ahorcado_5.png";  
+        case 4: return "img/ahorcado_4.png";
+        case 3: return "img/ahorcado_3.png";
+        case 2: return "img/ahorcado_2.png";
+        case 1: return "img/ahorcado_1.png"; 
+        case 0: return "img/ahorcado_0.png"; 
+    }
+}
+
+function random(array) {
+    let indice = Math.floor(Math.random() * (array.length - 1));
+    return array[indice];
+}
+
+function esLetra (letra) {
+    let reg = /[A-Za-z]/;
+
+    return reg.test(letra);
+}
 comenzarJuego();
